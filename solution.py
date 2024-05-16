73f985ae64c62006dc5af480656be3730172559c
KaravaevaAS <88322951+placeforyourad@users.noreply.github.com>
2024-05-16 14:25:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,9 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import ks_2samp
 
 chat_id = 422633669 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    mean_x = np.mean(x)
-    mean_y = np.mean(y)
-    
-    var_x = np.var(x)
-    var_y = np.var(y)
-    
-    t_stat = (mean_x - mean_y) / np.sqrt(var_x / len(x) + var_y / len(y))
-    
-    p_value = 2 * scipy.stats.t.cdf(-abs(t_stat), len(x) + len(y) - 2)
-    
-    return p_value < 0.05
+    stat, p = ks_2samp(x, y)
+    return p < 0.04 # Ваш ответ, True или False
